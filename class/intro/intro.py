191e2231a3edf0c39bf2b0106a368968429e6a94
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-09-07 10:14:00 +0800

    Learning Variables

--- class/intro/intro.py
+++ class/intro/intro.py
@@ -4,3 +4,10 @@ print(f"hi my name is {name}")
 name = "20"
 print(f"lalalala{name}")
 # bad practice dont change data types within a same variable
+age = 26
+if age < 18:
+    print("bruh")
+elif age < 21:
+    print("shush")
+else:
+    print("hooray")
---
+++ class/intro/variables/variables.py
@@ -0,0 +1,30 @@
+# Python is dynamically typed
+
+# integer
+
+myvar = 7
+print(myvar)
+
+# float
+
+myvar = 7.3
+print(myvar)
+
+# string
+myvar = "now i'm a string"
+print(myvar)
+
+myvar = "now i\'m a string"
+print(myvar)
+
+myvar = """
+dkla;fjdklsafjdkls;afjkdls;afjkdls;a
+ajkdlsajkdl;fja
+sjkdflsajdklf;sd
+ajdklfajdkl;f
+"""
+print(myvar)
+a, b = 5, 10
+print(f"a: {a}\t b:{b}")
+a, b = b, a
+print(f"a: {a}\t b:{b}")
