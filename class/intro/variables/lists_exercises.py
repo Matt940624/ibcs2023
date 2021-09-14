0d25b87abdaa3522892ff05f15106b0375ac2b01
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-09-14 10:15:27 +0800

    homework

--- class/intro/variables/lists_exercises.py
+++ class/intro/variables/lists_exercises.py
@@ -1,8 +1,24 @@
 # lists_exercises.py
 
 # 1. Sum all of the elements in the list
+mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sum = sum(mylist)
+print(sum)
 # 2. Write a program that removes all duplicates from a list
+mylist = [1, 1, 1, 2, 3, 3, 3, 4, 5]
+
 # 3. Write a program that finds the interesection of two lists
+mylist1 = [1, 1, 1, 2, 3, 3, 3, 4, 5]
+mylist2 = [2, 2, 2, 3, 4, 4, 5, 5, 6]
+
+
 # 4. Write a program the finds the union of two lists, omitting duplicates
+mylist1 = [1, 1, 1, 2, 3, 3, 3, 4, 5]
+mylist2 = [2, 2, 2, 3, 4, 4, 5, 5, 6]
+
 # 5. Write a program that finds the differences of two lists
+mylist1 = [1, 1, 1, 2, 3, 3, 3, 4, 5]
+mylist2 = [2, 2, 2, 3, 4, 4, 5, 5, 6]
+
 # 6. Write a program that creates a list containing the frequencies of elemtns in a list
+mylist = [1, 1, 1, 2, 3, 3, 3, 4, 5]
