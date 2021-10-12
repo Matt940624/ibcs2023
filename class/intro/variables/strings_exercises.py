86e4c9abcaa6fbce738f371223e66821494704ca
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-10-12 09:53:09 +0800

    added questions

---
+++ class/intro/variables/strings_exercises.py
@@ -0,0 +1,44 @@
+# 1
+# Write a python program to count and return a dictionary of character
+# frequencies in a given string
+
+# Sample String: 'google.com'
+# Expected Result: {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
+
+text = "google.com"
+freq = {}
+
+# 2
+# Write a Python function to insert a string in the middle of a string
+
+# Sample function and result:
+# insert_string_middle('[[]]', 'Python') -> [[Python]]
+# insert_string_middle('{{}}', 'PHP') -> {{PHP}}
+# insert_string_middle('|||', 'Unbalanced') -> |Unbalanced||
+wrapper = "(())"
+interior = "Python"
+
+
+# 3
+# Write a Python program to display a number with a comma separator.
+# Sample function and result:
+# comma_sep("1102") => 1,102
+# comma_sep("2340340958") => 2,340,340,958
+# Advanced: comma_sep("2340340958", 4) => 23,4034,0958
+number = "1102"
+
+
+# 4
+# Write a Python program to move spaces to the front of a given string.
+# Sample function and result:
+# front_pack("This is a sentence with several spaces") =>
+#       "      Thisisasentencewithseveralspaces"
+text = "This is a sentence with several spaces"
+
+# 5
+# Write a Pythong program to compute sum of digits of a given number string
+number = "394857"
+
+# 6 Write a Python program to determine if a set of parenthesis are balanced
+# (()((()(())())())))()))))(())())()
+text = "(()((()(())())())))()))))(())())()"
