a47d6104aef9827f98917a807ed01cefb40a123a
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-12-08 18:04:35 +0800

    enigma finished

--- class/intro/OOP/Enigma/Enigma.py
+++ class/intro/OOP/Enigma/Enigma.py
@@ -3,46 +3,100 @@ from typing import Any
 import numpy.random as random
 
 ENCODING = "UTF-8"
-PERMUTATION_SIZE = 8
+PERMUTATION_SIZE = 256
+ROTOR_DIMS = (10, 20)
 
 
 class Enigma:
     def __init__(self, seed: Any):
         random.seed(seed)
+        self._plugboard = Plugboard()
+        self._rotors = []
+        self._reflector = Reflector()
+        num_rotors = random.randint(ROTOR_DIMS[0], ROTOR_DIMS[1])
+        for _ in range(0, num_rotors):
+            self._rotors.append(Rotor())
 
     def encipher(self, val: int) -> int:
-        pass
+        # pass one way through the rotors
+        cipher = self._plugboard.encipher_forwards(val)
+        for idx, rotor in enumerate(self._rotors):
+            # always rotate first rotor
+            if idx == 0:
+                rotor.rotate()
+            elif self._rotors[idx-1].rotate_next():
+                rotor.rotate()
+            cipher = rotor.encipher_forwards(cipher)
+
+        cipher = self._reflector.encipher(cipher)
+        for rotor in self._rotors[::-1]:
+            cipher = rotor.encipher_backwards(cipher)
+        cipher = self._plugboard.encipher_backwards(cipher)
+        return cipher
 
 
 class Plugboard:
     def __init__(self) -> None:
-        pass
+        self._wires = [i for i in range(PERMUTATION_SIZE)]
+        swapped = []
+        for idx, val in enumerate(self._wires):
+            if idx not in swapped and random.randint(0, 9) <= 5:
+                # each wire will have a 60% chance of being swapped
+                # choose a random location to be swapped
+                loc = random.randint(0, len(self._wires)-1)
+                if loc not in swapped:
+                    tmp = self._wires[loc]
+                    self._wires[loc] = val
+                    self._wires[idx] = tmp
+                    swapped.insert(0, idx)
+                    swapped.insert(0, loc)
 
     def encipher_forwards(self, val: int) -> int:
-        pass
+        cipher = self._wires[val]
+        return cipher
 
     def encipher_backwards(self, val: int) -> int:
-        pass
+        cipher = self._wires.index(val)
+        return cipher
+
+    def __str__(self):
+        s = "Plugboard: \n"
+        for idx, val in enumerate(self._wires):
+            s = s + f"{idx}<->{val}\n"
+        return s
 
 
 class Rotor:
     def __init__(self) -> None:
-        pass
+        self._rotor_pos = 0
+        self._rotate_next_pos = random.randint(0, PERMUTATION_SIZE-1)
+        self._wires = [i for i in range(0, PERMUTATION_SIZE)]
+        random.shuffle(self._wires)
+        self.set_position(random.randint(0, PERMUTATION_SIZE-1))
 
     def set_position(self, position):
-        pass
+        change = self._wires.index(position) - self._rotor_pos
+        self._rotor_pos = position
+        self._wires = self._wires[change:]+self._wires[:change]
 
     def rotate(self):
-        pass
+        self._rotor_pos = (self._rotor_pos + 1) % PERMUTATION_SIZE
+        self._wires = self._wires[1:]+self._wires[:1]
 
     def rotate_next(self) -> bool:
-        pass
+        return self._rotor_pos == self._rotate_next_pos
 
     def encipher_forwards(self, val: int) -> int:
-        pass
+        cipher = self._wires[val]
+        return cipher
 
     def encipher_backwards(self, val: int) -> int:
-        pass
+        cipher = self._wires.index(val)
+        return cipher
+
+    def __str__(self):
+        s = f"Rotor\n\t{self._wires}\n\tRotor Position: {self._rotor_pos}\n\tRotate Next Position: {self._rotate_next_pos}"
+        return s
 
 
 class Reflector:
@@ -71,3 +125,16 @@ class Reflector:
 if __name__ == "__main__":
     r = Reflector()
     print(r._permutations)
+
+    p = Plugboard()
+    print(p)
+
+    r = Rotor()
+    print(r)
+
+    e = Enigma(100)
+    cipher = e.encipher(5)
+    print(cipher)
+    e = Enigma(100)
+    cipher = e.encipher(cipher)
+    print(cipher)
--- class/intro/OOP/Enigma/main.py
+++ class/intro/OOP/Enigma/main.py
@@ -1,8 +1,30 @@
 # main.py
-import numpy.random as random
+import os
+from Enigma import Enigma
+
+os.chdir(os.path.dirname(__file__))
+
+
+def encipher(seed: any, data: bytearray):
+    e = Enigma(seed)
+    encoded_buffer = bytearray()
+
+    for b in data:
+        encoded_byte = e.encipher(b)
+        encoded_buffer.append(encoded_byte)
+
+    return encoded_buffer
+
+
+def main():
+    s = "your mom"
+    data = bytearray(s.encode('UTF-8'))
+    ciphertext = encipher(100, data)
+    print(ciphertext)
+
+    plaintext = encipher(100, ciphertext)
+    print(plaintext)
 
 
 if __name__ == '__main__':
-    random.seed(100)
-    for i in range(5):
-        print(random.randint(1, 100))
+    main()
