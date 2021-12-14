82dea9bd1a3674a4ee85dfb1dccff48ccd09bba1
Matt940624 <71871298+Matt940624@users.noreply.github.com>
2021-12-14 08:48:28 +0800

    Update main.py

--- class/intro/OOP/Enigma/main.py
+++ class/intro/OOP/Enigma/main.py
@@ -5,7 +5,7 @@ from Enigma import Enigma
 os.chdir(os.path.dirname(__file__))
 
 
-def encipher(seed: any, data: bytearray):
+def encipher_string(seed: any, data: bytearray):
     e = Enigma(seed)
     encoded_buffer = bytearray()
 
@@ -14,7 +14,24 @@ def encipher(seed: any, data: bytearray):
         encoded_buffer.append(encoded_byte)
 
     return encoded_buffer
-
+def encipher_file(infile_path: str, outfile_path: str, seed: any):
+#Open in and out files in binary format
+    with open(infile_path, '') as infd, open(outfile_path, '') as outfd:
+#   Infinite loop we break out of when no more dtat is read
+        while True:
+#         read in 64 bytes of data. This is usually a lot more 
+           buffer = infd.read(64)
+#            If we are not at the end of the file
+        if buffer:
+#         Create bytearray to store data
+            enciphered_buffer = bytearray()
+#     encipher each value
+            for b in buffer:
+                encoded_byte = e.encipher(b)
+                enciphered_buffer.append(encoded_byte)
+            outfd.write(enciphered_buffer)
+         else: 
+            break
 
 def main():
     s = "your mom"
